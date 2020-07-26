11b64ee94d953905247de6083411bcc060d0f953
Jake Massoth <jake@massoth.tech>
2020-07-26 16:48:10 -0500

    update to non-depreciated libraries

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,29 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
+Vue.use(VueRouter);
 
-const routerOptions = [
-  { path: '/', component: 'Home' },
-  { path: '/about', component: 'About' },
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
 ];
 
-const routes = routerOptions.map(route =>
-  ({
-    ...route,
-    component: () => import(`@/components/${route.component}.vue`),
-  }),
-);
-
-Vue.use(Router);
-
-export default new Router({
-  routes,
+const router = new VueRouter({
   mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
 });
+
+export default router;
--- main.py
+++ main.py
@@ -8,3 +8,6 @@ app = Flask(__name__,
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
