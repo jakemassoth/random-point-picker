fe914ace0bf25e229072a761fbfb2912847931d3
Jake Massoth <jake@massoth.tech>
2020-07-29 15:57:23 -0500

    add google fonts

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,4 +8,16 @@
 <style>
 @import 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css';
 @import 'https://fonts.googleapis.com/icon?family=Material+Icons';
+@import url('https://fonts.googleapis.com/css2?family=Lato');
+
+html {
+  height: 100%;
+  overflow:hidden;
+}
+
+body {
+  border: 0; margin: 0; padding: 0;
+  font-family: 'Lato', sans-serif;
+  height: 100%;
+}
 </style>
