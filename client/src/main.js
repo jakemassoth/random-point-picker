c03be7d95084ca8e01703e000cd2b9e60e84f888
Jake Massoth <jake@massoth.tech>
2020-07-29 20:33:41 -0500

    add VueSub

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import VueSub from 'vue-sub';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueSub);
+
+const observable = new VueSub();
+
 new Vue({
   router,
+  observable,
   render: (h) => h(App),
 }).$mount('#app');
