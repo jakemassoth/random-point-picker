dcd16723b19328a9d6d0b9d0580ff08687f83e18
Jake Massoth <jake@massoth.tech>
2020-08-05 15:36:36 -0500

    why did I do this

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,10 @@
 import Vue from 'vue';
-import VueSub from 'vue-sub';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 
-Vue.use(VueSub);
-
-const observable = new VueSub();
-
 new Vue({
   router,
-  observable,
   render: (h) => h(App),
 }).$mount('#app');
